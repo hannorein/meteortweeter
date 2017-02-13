3d416c4beb88550118a1581e31a07048b9a12666
Hanno Rein <hanno@hanno-rein.de>
2017-02-12 19:14:36 -0500

    check if any new files exist

--- run.bash
+++ run.bash
@@ -5,12 +5,16 @@ DIRNAME=`date -u +'%Y%m%d'`
 scp hrein@rein008.utsc.utoronto.ca:~/events/$DIRNAME/\*.avi todaysmeteors
 LATESTPROC=`cat latest.txt`
 LATEST=`ls todaysmeteors | tail -n 1`
+LATESTLEN=${#LATEST} 
 if [ "$LATESTPROC" == "$LATEST" ]; then
     echo "no new match"
-
 else
-    echo "new match"
-    echo "$LATEST" >latest.txt
-    avconv -y -re -r:v 25  -i todaysmeteors/$LATEST -s 1280x720 latest.mp4
-    ./uploadVideoToTwitter.bash
+    if [ "$LATESTLEN" -lt "5" ]; then
+        echo "no new files"
+    else
+        echo "new match"
+        echo "$LATEST" >latest.txt
+        avconv -y -re -r:v 25  -i todaysmeteors/$LATEST -s 1280x720 latest.mp4
+        ./uploadVideoToTwitter.bash
+    fi
 fi
