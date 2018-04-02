8931d686100d54bd10d86010eb32b05ff123e1fd
Hanno Rein <hanno@hanno-rein.de>
2018-04-02 14:40:33 -0400

    fixed new file format change

--- run.bash
+++ run.bash
@@ -2,7 +2,9 @@
 rm -rf todaysmeteors
 mkdir todaysmeteors
 DIRNAME=`date -u +'%Y%m%d'`
-scp hrein@rein008.utsc.utoronto.ca:~/events/$DIRNAME/\*.avi todaysmeteors
+#DIRNAME='20180401'
+#scp hrein@142.1.98.162:~/events/$DIRNAME/\*.avi todaysmeteors
+scp hrein@rein008.utsc.utoronto.ca:~/events/$DIRNAME/\*.mp4 todaysmeteors
 LATESTPROC=`cat latest.txt`
 LATEST=`ls todaysmeteors | tail -n 1`
 LATESTLEN=${#LATEST} 
@@ -14,7 +16,7 @@ else
     else
         echo "new match"
         echo "$LATEST" >latest.txt
-        avconv -y -re -r:v 25  -i todaysmeteors/$LATEST -s 1280x720 latest.mp4
+        avconv -y -re -r:v 25  -i todaysmeteors/$LATEST -s 640x480 latest.mp4
         ./uploadVideoToTwitter.bash
     fi
 fi
